187f42f61aebe088b4150c5cf4aef319b995ef42
osaaso2 <66313255+osaaso2@users.noreply.github.com>
2023-08-12 01:02:46 -0400

    Delete python_actr directory

--- python_actr/__init__.py
+++
@@ -1,7 +0,0 @@
-from .model import Model, log_everything
-from .production import ProductionSystem
-from .logger import log, finished
-from .runner import run, run_with
-from .display import display
-from .actr import *
-from . import version
--- python_actr/display/tk/__init__.py
+++
@@ -1,9 +0,0 @@
-from .default import DefaultRenderer
-
-def render(obj,canvas):
-    if not hasattr(obj,'_display'):
-        obj._display=DefaultRenderer(obj,canvas)    
-    obj._display.render(canvas)
-    for c in obj.get_children():
-        render(c,canvas)
-        
