96ef574243c200d501d086efd47d0e5138db2a5f
osaaso2 <66313255+osaaso2@users.noreply.github.com>
2023-08-12 00:32:29 -0400

    Delete python_actr directory

--- python_actr/actr/text.py
+++
@@ -1,6 +0,0 @@
-import python_actr
-
-class TextOutput(python_actr.Model):
-    def write(self,text):
-        print(text)
-        self.log._=text
--- python_actr/version.py
+++
@@ -1,6 +0,0 @@
-name = "python_actr"
-version_info = (1, 9, 2)
-dev = True
-version = '.'.join([str(x) for x in version_info])
-if dev:
-    version = version + 'dev'
