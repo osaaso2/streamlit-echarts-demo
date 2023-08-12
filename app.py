babad80baa6ba263327788bd26c637fa5695f910
osaaso2 <66313255+osaaso2@users.noreply.github.com>
2023-08-12 01:42:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,45 @@ from demo_pyecharts import ST_PY_DEMOS
 import python_actr
 from python_actr import *
 print("as900000000000000000")
-log=log(html=True)
+#log=log(html=True)
 
+import logging
+import unittest
+
+import HTMLTestRunner
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+class BasicTestCase(unittest.TestCase):
+    def test_one(self):
+        logger.info('Test message 1!')
+        self.assertEqual(1, 1)
+
+    def test_two(self):
+        """Extended description"""
+        logger.error('Test message 1!')
+        self.assertEqual(2, 2)
+
+
+if __name__ == '__main__':
+    import sys
+    logging.basicConfig(stream=sys.stderr)
+
+    with open('report.html', 'w') as report_file:
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=report_file,
+            title='Test Report',
+            description='Regression Test Suite',
+            verbosity=3,
+            logger=logger
+        )
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(BasicTestCase)
+
+        result = runner.run(suite)
+        print(result)
 class MyEnvironment(Model):
     pass
 
