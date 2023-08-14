cae4e41464cd0a295974b0a75e80b48309c9838a
osaaso2 <66313255+osaaso2@users.noreply.github.com>
2023-08-13 21:40:30 -0400

    Update __init__.py

--- python_actr/__init__.py
+++ python_actr/__init__.py
@@ -2,7 +2,6 @@ from .model import Model, log_everything
 from .production import ProductionSystem
 from .logger import log, finished
 from .runner import run, run_with
-from .ex1 import MyEnvironment, MyAgent
 from .display import display
 from .actr import *
 from . import version
