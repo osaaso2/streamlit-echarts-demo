5dd0cb7f5f8378f85974bad6834c66a3c52143af
osaaso2 <66313255+osaaso2@users.noreply.github.com>
2023-08-13 20:27:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
     #logger.error='Test message 1!'
     program = 'a = 5\nb=10\nprint("Sum =", a+b)'
     exec(program)
-    python_actr/tests/test_actr_compile.py
+    #python_actr/tests/test_actr_compile.py
     #st.title(log.error1)
     
     with st.sidebar:
