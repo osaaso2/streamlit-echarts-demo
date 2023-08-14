fe598ea449ea1ed23726f68865a2266fb5486b9d
osaaso2 <66313255+osaaso2@users.noreply.github.com>
2023-08-13 21:39:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,42 @@ print("as900000000000000000")
 #logger.setLevel(logging.DEBUG)
 #import unittest
 import sys
+
+log.as1="as1log"
+
+class MyEnvironment(Model):
+    pass
+log.as1="as1"
+#####
+# create an act-r agent
+
+class MyAgent(ACTR):
+    
+    focus=Buffer()
+    focus.set('sandwich bread')
+    log.as1="aso111"
+    #log.as2="aaa"
+    def bread_bottom(focus='sandwich bread'):     # if focus buffer has this chunk then....
+        print("I have a piece of bread")           # print
+        log.as1='goal1'
+        focus.set('sandwich cheese')              # change chunk in focus buffer
+
+    def cheese(focus='sandwich cheese'):          # the rest of the productions are the same
+        print ("ended")    # but carry out different actions
+        log.as2='goal2'
+        focus.set('stop')
+    def stop_production(focus='stop'):
+        self.stop()                        # stop the agent
+##
+##    def ham(focus='sandwich ham'):
+##        print "I have put  ham on the cheese"
+##        focus.set('sandwich bread_top')
+##
+##    def bread_top(focus='sandwich bread_top'):
+##        print "I have put bread on the ham"
+##        print "I have made a ham and cheese sandwich"
+##        focus.set('stop')   
+##
 log.as1="000"
 #from python_actr import ex1
 #from  import MyEnvironment, MyAgent
